bfcf29ceb96411ee43db5a7eb4f5576e8dea7b05
Shourya <shouryab2001@gmail.com>
2021-06-30 12:18:05 +0530

    Third commit : Added more action (post) to the users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 const userController = require('../controllers/user_constroller');
 
 router.get('/profile',userController.profile);
+router.get('/post',userController.post);
 
 module.exports = router;
