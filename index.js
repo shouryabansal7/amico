47ebf82dfabb334119af6a0067a7c0ebf67e91f8
Shourya <shouryab2001@gmail.com>
2021-06-30 18:07:37 +0530

    fourth commit : Create Layout and use Partials

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port, function(err){
     if(err){
         //console.log('error:',err);
