b63c962c4a446d466989e1674712cf266e7b9ec9
Shourya <shouryab2001@gmail.com>
2021-07-09 20:24:51 +0530

    eleventh commit : display posts with the name of the user (who added that post)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,23 @@
+const Post = require('../models/posts');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
+    // to display posts
+    /*Post.find({},function(err,posts){
+            return res.render('home',{
+            title: 'Amico | Home',
+            posts: posts
+        });
+    });*/
+   
+   /* return res.render('home',{
         title: 'Home'
+    });*/
+
+    //to display posts along wiht the user's name who made the post
+    Post.find({}).populate('user').exec(function(err,posts){
+            return res.render('home',{
+            title: 'Amico | Home',
+            posts: posts
+        });
     });
 };
