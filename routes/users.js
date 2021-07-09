8237c4913a26d3c811f24cda959bb6d6ef16d1ce
Shourya <shouryab2001@gmail.com>
2021-07-09 21:27:32 +0530

    twelfth commit : to add authentication check (with authentication user cannot add a post)

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 const userController = require('../controllers/user_constroller');
 
-router.get('/profile',passport.checkAuthenticity ,userController.profile);
+router.get('/profile',passport.checkAuthenticity,userController.profile);
 router.get('/post',userController.post);
 router.get('/sign-up',userController.SignUp);
 router.get('/sign-in',userController.SignIn);
