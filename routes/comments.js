4898d27bc913c081ff1d1b2756cd3e0a4c6e0780
Shourya <shouryab2001@gmail.com>
2021-07-13 19:27:55 +0530

    sixteenth commit : delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,6 @@ const passport = require('passport');
 const commentsController = require('../controllers/comment_controller');
 
 router.post('/create',passport.checkAuthenticity,commentsController.create);
-
+router.get('/destroy/:id',passport.checkAuthenticity,commentsController.destroy);
 
 module.exports = router;
